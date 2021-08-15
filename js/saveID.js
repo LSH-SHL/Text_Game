9083640b3b4951b7523bb71f321f8abaed51b3e8
LSH-SHL <60726651+LSH-SHL@users.noreply.github.com>
2021-08-15 12:35:21 +0900

    Create saveID.js

---
+++ js/saveID.js
@@ -0,0 +1,60 @@
+var inputs = $('input[type="text"]');
+var googleSubmitBtn = $('#google-submit');
+var snackbar = $('#snackbar');
+
+var inputID = $('#name');
+
+function isLoading(status){
+  if(status){
+    $('html, body').addClass('wait');
+    googleSubmitBtn.attr('disabled', true).html('입력중...');
+  } else {
+    $('html, body').removeClass('wait');
+    googleSubmitBtn.attr('disabled', false).html('입력');
+  }
+}
+
+function checkInput(){
+  var isEmpty = false;
+  $.each(inputs, function (index, element) {
+    if (element.value === '') {
+      alert('빈 칸이 있어요.');
+      isEmpty = true;
+      return false;
+    }
+  });
+  return isEmpty;
+}
+
+$('#google-submit').click(function () {
+
+  //빈값 체크
+  if (checkInput()) { return; }
+
+  // 입력중..
+  isLoading(true);
+
+  $.ajax({
+    type: "GET",
+    url: "https://script.google.com/macros/s/AKfycbyI2qcgqnxd1lXbD4uKdfOpQTCmynjFAfqKqPXWnWjCymg5Ps3YigqFfrmofaq22X3Y8A/exec",
+    data: {
+      "이름": inputID.val(),
+    },
+    success: function (response) {
+      isLoading(false);
+
+      snackbar.html('입력이 완료됐습니다.').addClass('show');
+      setTimeout(function () {
+        snackbar.removeClass('show');
+      }, 3000);
+
+      //값 비워주기
+      inputID.val('');
+    },
+    error: function (request, status, error) {
+      isLoading(false);
+      console.log("code:" + request.status + "\n" + "error:" + error);
+      console.log(request.responseText);
+    }
+  });
+});
